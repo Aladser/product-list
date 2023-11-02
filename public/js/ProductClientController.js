f62bf1ac63399e4c5d0a16b814a4126539893338
Aladser <aladser@gmail.com>
2023-11-02 16:18:07 +0900

    обновлен клиентский контроллер товаров

--- public/js/ProductClientController.js
+++ public/js/ProductClientController.js
@@ -3,11 +3,16 @@ class ProductClientController extends ClientController {
     // добавить запись в БД
     add(form, event) {
         event.preventDefault();
+        let type = form.getAttribute('data-type');
 
         // действия после успешного добавления данных в БД
         let process = (data) => {
-            console.log(data);
-            this.msgElement.textContent = data.result == 1 ? 'товар изменен' : data.description;
+            //console.log(data);
+            if (data.result == 1) {
+                this.msgElement.textContent = type == 'add' ? 'товар добавлен' : 'данные обновлены';
+            } else {
+                this.msgElement.textContent = data.description;
+            }
         };
 
         // данные формы
