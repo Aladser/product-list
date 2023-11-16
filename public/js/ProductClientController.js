4d9f2f038f77af670c814d133a69fbc16d917e18
Aladser <aladser@gmail.com>
2023-11-16 22:56:00 +0900

    обновление ProductClientController

--- public/js/ProductClientController.js
+++ public/js/ProductClientController.js
@@ -1,4 +1,19 @@
-/** Фронт-контроллер таблицы */
+/** Фронт-контроллер таблицы 
+  *
+  GET|HEAD        product ...................product.index › ProductController@index
+  *
+  POST            product ..................product.store › ProductController@store
+  *
+  GET|HEAD        product/create ...........product.create › ProductController@create
+  *
+  GET|HEAD        product/{product} ........product.show › ProductController@show
+  *
+  PUT|PATCH       product/{product} ........product.update › ProductController@update
+  *
+  DELETE          product/{product} ........product.destroy › ProductController@destroy
+  *
+  GET|HEAD        product/{product}/edit ...product.edit › ProductController@edit
+*/
 class ProductClientController {
     constructor(URL, table, msgElement, addForm = null, editForm = null) {
         this.URL = URL;
@@ -28,12 +43,12 @@ class ProductClientController {
         }
         // форма изменения товара
         if (this.editForm) {
-            this.editForm.onsubmit = (event) => this.add(editForm, event);
+            this.editForm.onsubmit = event => this.update(event);
             this.editFormId = editForm.id;
         }
     }
 
-    // добавить запись в БД
+    // добавить новый товар в БД
     add(event) {
         event.preventDefault();
 
@@ -89,10 +104,69 @@ class ProductClientController {
         );
     }
 
-    update(form, event) {
+    // обновить товар в БД
+    update(event) {
+        event.preventDefault();
 
+        let id = this.editForm.getAttribute('data-id');
+        let data = {};
+        data.id = id;
+        // артикул
+        if (this.editForm.articul) {
+            data.articul = this.editForm.articul
+        } else {
+            data.articul = document.querySelector('#form-edit-product').textContent;
+        }
+        // имя
+        data.name = this.editForm.name.value;
+        // статус
+        data.status = this.editForm.status.value;
+        // атрибуты
+        let attrMap = new Map();
+        let attributesElements = document.querySelectorAll(
+            ".form-edit-product__attribute"
+        );
+        if (attributesElements.length > 0) {
+            attributesElements.forEach((element) => {
+                let name = element.querySelector(
+                    ".form-edit-product__attr-name"
+                ).value;
+                let value = element.querySelector(
+                    ".form-edit-product__attr-value"
+                ).value;
+                if (name !== "" && value !== "") {
+                    attrMap.set(name, value);
+                }
+            });
+        }
+        data.data = JSON.stringify(Object.fromEntries(attrMap));
+        
+        // обработка результата запроса
+        let process = (data) => {
+            if (data.result == 1) {
+                this.msgElement.textContent = 'данные обновлены';
+            } else {
+                this.msgElement.textContent = data.description;
+            }
+        };
+    
+        let headers = {
+            "X-CSRF-TOKEN": this.csrfToken.getAttribute("content"),
+            'Content-Type': 'application/json'
+        };
+
+        // запрос на сервер
+        ServerRequest.execute(
+            `/product/${id}`,
+            process,
+            "patch",
+            this.msgElement,
+            JSON.stringify(data),
+            headers
+        );
     }
 
+    // удалить товар в БД
     remove(row) {
         let id = row.id;
         id = id.slice(id.indexOf("-") + 1);
@@ -120,9 +194,5 @@ class ProductClientController {
             null,
             headers
         );
-    }
-
-    processData(row, form) {
-        alert("нет реализации метода processData класса TableFrontController");
     }
 }
--- public/js/edit-product.js
+++ public/js/edit-product.js
@@ -1,73 +1,9 @@
-/*
- GET|HEAD        product ...................product.index › ProductController@index
-  POST            product ..................product.store › ProductController@store
-  GET|HEAD        product/create ...........product.create › ProductController@create
-  GET|HEAD        product/{product} ........product.show › ProductController@show
-  PUT|PATCH       product/{product} ........product.update › ProductController@update
-  DELETE          product/{product} ........product.destroy › ProductController@destroy
-  GET|HEAD        product/{product}/edit ...product.edit › ProductController@edit
-*/
-
 const formClass = 'form-edit-product';
-const msgElement = document.querySelector("#table-error");
-const form = document.querySelector(".form-edit-product");
-const id = form.getAttribute('data-id');
-const csrfToken = document.querySelector('meta[name="csrf-token"]');
-let headers = {
-    "X-CSRF-TOKEN": csrfToken.getAttribute("content"),
-    'Content-Type': 'application/json'
-};
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    let data = {};
-    data.id = id;
-    // артикул
-    if (form.articul) {
-        data.articul = form.articul
-    } else {
-        data.articul = document.querySelector('#form-edit-product').textContent;
-    }
-    // имя
-    data.name = form.name.value;
-    // статус
-    data.status = form.status.value;
-    // атрибуты
-    let attrMap = new Map();
-    let attributesElements = document.querySelectorAll(
-        ".form-edit-product__attribute"
-    );
-    if (attributesElements.length > 0) {
-        attributesElements.forEach((element) => {
-            let name = element.querySelector(
-                ".form-edit-product__attr-name"
-            ).value;
-            let value = element.querySelector(
-                ".form-edit-product__attr-value"
-            ).value;
-            if (name !== "" && value !== "") {
-                attrMap.set(name, value);
-            }
-        });
-    }
-    attr = JSON.stringify(Object.fromEntries(attrMap));
-    data.data = attr;
-    
-    // запрос на сервер
-    ServerRequest.execute(
-        `/product/${id}`,
-        process,
-        "patch",
-        msgElement,
-        JSON.stringify(data),
-        headers
-    );
-});
-// обработка результата запроса
-let process = (data) => {
-    if (data.result == 1) {
-        msgElement.textContent = 'данные обновлены';
-    } else {
-        msgElement.textContent = data.description;
-    }
-};
+const tableController = new ProductClientController(
+    "/product",
+    null,
+    document.querySelector("#table-error"),
+    null,
+    document.querySelector(`.${formClass}`)
+);
