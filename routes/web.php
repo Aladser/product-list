8c60e1262929830d624ae9fb5b5a8f345fd4f9ca
Aladser <aladser@gmail.com>
2023-11-17 11:32:43 +0900

    обновление роутинга

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,5 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 require __DIR__.'/auth.php';
-// Route::get('/', fn () => view('welcome'))->name('main');
-
-Route::get('/', function () {
-    return view('welcome');
-})->name('main');
-
+Route::get('/', fn () => view('welcome'))->name('main');
 Route::resource('/product', ProductController::class);
